6ff4c46b931e04bb57c5f5c552604f961e929012
Lnismeee <langocltr02@gmail.com>
2024-07-13 22:05:43 +0700

    ngoc

--- src/pages/RegisterPage/RegisterPage.jsx
+++ src/pages/RegisterPage/RegisterPage.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const RegisterPage = () => {
-  return <div>RegisterPage</div>;
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt itaque
+      ipsum explicabo, non maiores iure nam neque culpa sequi omnis dolores
+      numquam quas inventore rem magni illo doloribus et possimus, delectus
+      voluptatum velit, veritatis sunt adipisci? Ex id rem non. Facilis sapiente
+      debitis ex maxime fugit commodi asperiores, veniam fugiat!
+    </div>
+  );
 };
 
 export default RegisterPage;

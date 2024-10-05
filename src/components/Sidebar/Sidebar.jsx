ede904e66006421dc1b1641a5885d24dc76833e1
LePhuNhan <lephunhan2401@gmail.com>
2024-10-05 23:33:05 +0700

    Update Sidebar.jsx

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -4,17 +4,14 @@ import { Link } from "react-router-dom";
 import axios from "axios";
 import "./Sidebar.style.css";
 
-
-
 const Sidebar = () => {
   const [references, setReferences] = useState([]);
   const { Text } = Typography;
   const DomainApi = process.env.REACT_APP_DOMAIN_API;
-  const [rankPlayers, setRankPlayers] = useState([])
+  const [rankPlayers, setRankPlayers] = useState([]);
   const [infoPlayer, setInfoPlayer] = useState([]);
   const accessToken = localStorage.getItem("accessToken");
 
- 
   useEffect(() => {
     const fetchReferences = async () => {
       const topicId = localStorage.getItem("selectedTopicId");
@@ -24,7 +21,7 @@ const Sidebar = () => {
             `${DomainApi}/documentation/topic/${topicId}`
           );
           const referencesData = response.data;
-          
+
           const shuffled = referencesData.sort(() => 0.5 - Math.random());
           const fiveRandomReferences = shuffled.slice(0, 5);
 
@@ -32,8 +29,7 @@ const Sidebar = () => {
         } catch (error) {
           console.error("Error fetching references:", error);
         }
-      }
-      else {
+      } else {
         setReferences(null);
       }
     };
@@ -50,7 +46,7 @@ const Sidebar = () => {
               Authorization: `Bearer ${accessToken}`,
             },
           });
-          setInfoPlayer(response.data)
+          setInfoPlayer(response.data);
         } catch (error) {
           console.error("Failed to fetch player data:", error);
         }
@@ -62,14 +58,14 @@ const Sidebar = () => {
 
   const fetchRankPlayers = async () => {
     try {
-        await axios.get(`${DomainApi}/player/rank`).then((response) => {
-            setRankPlayers(response.data);
-        });
+      await axios.get(`${DomainApi}/player/rank`).then((response) => {
+        setRankPlayers(response.data);
+      });
     } catch (error) {
-        console.log(error);
+      console.log(error);
     }
-};
-useEffect(() => {
+  };
+  useEffect(() => {
     fetchRankPlayers();
   }, [rankPlayers]);
 
@@ -77,29 +73,45 @@ useEffect(() => {
     <div style={{ width: "40%" }} className="responsive-hide">
       <Card title="Leaderboards!" style={{ marginBottom: "16px" }}>
         <ul className="listRankPlayer">
-          {rankPlayers.length !== 0 && rankPlayers.map((item,index) =>{
-            return(
-              <li key={index} className={item.email === infoPlayer.email ? 'itemRankPlayer active': 'itemRankPlayer'}>#{index+1} 
-              <span className="fullname">{item.fullname}</span>
-              <span>{item.totalScore !== null ? item.totalScore : 0} pts</span>
-              </li>
-            )
-          })}
+          {rankPlayers.length !== 0 &&
+            rankPlayers.map((item, index) => {
+              return (
+                <li
+                  key={index}
+                  className={
+                    item.email === infoPlayer.email
+                      ? "itemRankPlayer active"
+                      : "itemRankPlayer"
+                  }
+                >
+                  #{index + 1}
+                  <span className="fullname">{item.fullname}</span>
+                  <span>
+                    {item.totalScore !== null ? item.totalScore : 0} pts
+                  </span>
+                </li>
+              );
+            })}
         </ul>
       </Card>
       <Card title="The references you should read" className="listRef">
-        <List
-          size="small"
-          dataSource={references}
-          renderItem={(item, index) => (
-            <List.Item key={index}>
-              <Link to={`/documentDetail/${item._id}`}>
-                <Text className="references">{item.name}</Text>
-              </Link>
-            </List.Item>
-          )}
-        />
+        {references && references.length > 0 ? (
+          <List
+            size="small"
+            dataSource={references}
+            renderItem={(item, index) => (
+              <List.Item key={index}>
+                <Link to={`/documentDetail/${item._id}`}>
+                  <Text className="references">{item.name}</Text>
+                </Link>
+              </List.Item>
+            )}
+          />
+        ) : (
+          <Text>No references available to display.</Text>
+        )}
       </Card>
+
       <Row gutter={[16, 16]} className="flex-container">
         <Link to="/">
           <Col className="flex-item">
